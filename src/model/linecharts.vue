5e4ae94c4772813ecffc3eaa1ed27cf2d896e608
xzh.hf <xzh.hf@hangshutech.com>
2019-05-27 19:21:01 +0800

    1.0.0 添加折线图模型

---
+++ src/model/linecharts.vue
@@ -0,0 +1,24 @@
+<!-- 折线图模型 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  created: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
